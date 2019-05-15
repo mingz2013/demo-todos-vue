aec0e840c07babf8c9641015fac7c5718481ee74
mingz <305603665@qq.com>
2019-05-15 11:36:36 +0800

    store

--- src/main.js
+++ src/main.js
@@ -1,21 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './components/App'
-import router from './router'
+
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-
-import store from './store'
-
-import Vuex from 'vuex'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import router from './router'
+import store from './store'
+
+import App from './components/App'
+
 Vue.use(VueAxios, axios)
 Vue.use(iView)
-Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
-
 import todosModule from './modules/todos'
 import countModule from './modules/count'
+
+Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
